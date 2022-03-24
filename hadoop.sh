83efa2bc6bf9a52e0c0eaa390d8a620cd5b76ef2
Ubuntu <ubuntu@ip-172-31-59-233.ap-northeast-2.compute.internal>
2022-03-24 08:41:14 +0000

    0324-new

--- hadoop.sh
+++ hadoop.sh
@@ -4,12 +4,12 @@ hadoop fs -rm -R /input
 
 hadoop fs -rm -R /output
 
-hadoop fs -put /analy1/result/${2}_wc.csv /input
+hadoop fs -put /word_count/result/${1}_wc.csv /input
 
 timestamp=`date "+%Y%m%d%H%M"`
 
 hadoop jar /usr/hadoop/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.10.1.jar wordcount /input /output
 
-hadoop fs -get /output/part-r-00000 /analy1/result/${2}_${timestamp}
+hadoop fs -get /output/part-r-00000 /word_count/result/${1}_${timestamp}
 
 echo "$timestamp"
